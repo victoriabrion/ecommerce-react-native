c6feb08205641d06ea3af7bc420fb222503934fc
victoria brion <victoriabrion7@gmail.com>
2024-02-22 18:34:36 -0300

    added navigation folder

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
-import { StyleSheet, StatusBar} from 'react-native'
+import {StatusBar} from 'react-native'
 import { useFonts } from "expo-font"
 import { fontCollection } from './src/utils/globals/fonts'
 import colors from './src/utils/globals/colors'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Home from './src/screens/Home'
-import ItemListCategory from './src/screens/ItemListCategory'
-import ItemDetail from './src/screens/ItemDetail'
-import Header from './src/components/Header'
-
-const Stack = createNativeStackNavigator()
+import MainNavigatior from './src/components/navigation/MainNavigatior'
 
 const App = () => {
 
@@ -21,26 +14,10 @@ const App = () => {
   return (
     <>
     <StatusBar backgroundColor={colors.tertiary}  barStyle= 'light-content'/>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home' screenOptions={({route, navigation}) => {
-                                                  return { 
-                                                    header: () => {
-                                                      return <Header navigation = {navigation}
-                                                                     title={route.name === 'Home' ? 'Store' : 
-                                                                            route.name === 'ItemListCategory' ? route.params.categorySelected : 'More info'}/>}}}}>
-        <Stack.Screen name= 'Home' component= {Home}></Stack.Screen>
-        <Stack.Screen name= 'ItemListCategory' component= {ItemListCategory}></Stack.Screen>
-        <Stack.Screen name= 'ItemDetail' component= {ItemDetail}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <MainNavigatior />
     </>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({
-  container: {
-    flex:1
-  }
-})
